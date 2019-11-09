dbca29cc9da76ea9dc31ba0bd3d359020accdef7
Liz P <parisielizabeth@gmail.com>
2019-11-09 04:27:40 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  get '/articles/edit/:id', to: 'articles#edit', as: :edit_article
+  get 'articles/edit/:id', to: 'articles#edit', as: :edit_article
+  
+  patch 'articles/:id', to: 'articles#update'
 end
